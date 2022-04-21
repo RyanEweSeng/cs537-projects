4da18bb6f99652c8d8adff15d61a106653cc33cc
RyanEweSeng <ryantoh1001@gmail.com>
2022-04-20 22:24:03 -0500

    Add queue structure, queue size, and head pointer

--- p4/proc.h
+++ p4/proc.h
@@ -49,6 +49,9 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  char* queue[CLOCKSIZE];
+  int queue_head;
+  int queue_size;
 };
 
 // Process memory is laid out contiguously, low addresses first:
